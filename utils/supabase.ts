d0e83cdf0a9ad3f536a9aeea11efcedc3f08f9bb
Hakan Çiçek <hakancicek@bdc.wls.metu.edu.tr>
2026-03-06 14:20:47 +0300

    fix: update supabase anon key JWT

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Vercel'e eklediğimiz ortam değişkenlerini çekiyoruz
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://wfbjinfihduwgbuhwmeo.supabase.co';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_BI0BSm5EPMAq2jAsMFMJjA_SxJd_w0O';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmYmppbmZpaGR1d2didWh3bWVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3MTYzOTcsImV4cCI6MjA4ODI5MjM5N30.cL9j3Enj6LZ_Cd-Ki7ox3IE9gMB633b9XcQUzqE09G8';
 // Köprüyü kuruyoruz
 export const supabase = createClient(supabaseUrl, supabaseKey);
